3470c9cd3152e6e9db1f1984981d39bb45e89344
Ando <andtsatur1296@gmail.com>
2021-03-22 19:12:28 +0400

    added initial count and decrease buttons

--- src/row/index.js
+++ src/row/index.js
@@ -2,15 +2,25 @@ import {useState, useCallback} from 'react';
 export default function Row({language}){
   const [numberOfVotes, setNumberOfVotes] = useState(0);
 
-  const addHandler = useCallback(() => {
-    setNumberOfVotes(prevState => prevState + 1)
-  },[])
+  const addHandler = useCallback((type) => {
+    if(type === 'increase') {
+      setNumberOfVotes(prevState => prevState + 1)
+    } else if(type === 'decrease') {
+      if(numberOfVotes !== 0) {
+        setNumberOfVotes(prevState => prevState - 1);
+      }
+    } else {
+      setNumberOfVotes(0);
+    }
+  },[numberOfVotes]);
   return (
     <>
       <td>{language}</td>
       <td>{numberOfVotes}</td>
       <td>
-        <button onClick={addHandler}>Add</button>
+        <button onClick={() => addHandler('increase')}>Add</button>
+        <button onClick={() => addHandler('decrease')}>down</button>
+        <button onClick={() => addHandler('reset')}>reset</button>
       </td>
     </>
   )
